83db452e9e1caf911b68638e8a1207340f2ae6e3
Nils Bergemann <nils.bergemann@icloud.com>
2025-04-04 23:08:44 +0200

    Remove old code

--- Source/GorgeousEventsRuntime/Public/Events/SubEvents/GorgeousSubEvent.h
+++ Source/GorgeousEventsRuntime/Public/Events/SubEvents/GorgeousSubEvent.h
@@ -6,6 +6,25 @@
 |              administrated by Epic Nova. All rights reserved.             |
 | ------------------------------------------------------------------------- |
 |                   Epic Nova is an independent entity,                     |
-|      that is has nothing in common with Epic Games in any capacity.       |
+|         that has nothing in common with Epic Games in any capacity.       |
 <==========================================================================*/
 #pragma once
+
+#include "GorgeousSubEvent.generated.h"
+
+UCLASS(Abstract, Blueprintable, BlueprintType, Category = "Gorgeous Events|Sub Events", ClassGroup = "Gorgeous Events", EditInlineNew, Experimental, NotPlaceable,
+	meta = (ToolTip = "The base class for all Gorgeous Sub Events."))
+class UGorgeousSubEvent : public UGorgeousEvent
+{
+	GENERATED_BODY()
+	
+	friend class UGorgeousEvent;
+
+public:
+
+	//The state that the executing event needs for this event to be kicked off.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Gorgeous Events")
+	EGorgeousEventState_E RunOnParentState;
+};
+
+//@TODO: Subevents are events that use the data from the invoking event to satisfy their execution. So they theoretically don't need extra data and can be invoked by the parent event at any given time
